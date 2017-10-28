831d75dff2ac619c6ddbde35bd0b3d33b2fc34c4
Achintya Gopal <achintyagopal@hotmail.com>
2017-10-28 13:14:24 -0400

    Updated predict and programmed LSTM

--- cloze/hw4/model.py
+++ cloze/hw4/model.py
@@ -202,3 +202,105 @@ class BiGRU(nn.Module):
 
     def init_hidden(self, batch_size):
         return Variable(torch.zeros(batch_size, self.hidden_size))
+
+class BiLSTM(nn.Module):
+
+    def __init__(self, vocab_size, hidden_size = 8, embedding_size=32, cell_size = 8, dropout=0.4):
+        super(BiLSTM, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.embedding_size = embedding_size
+        self.cell_size = cell_size
+        self.dropout = dropout
+
+        self.embeddings = nn.Parameter(torch.randn(vocab_size, embedding_size), requires_grad=True)
+        self.W_f1 = nn.Linear(embedding_size + hidden_size, 1)
+        self.W_i1 = nn.Linear(embedding_size + hidden_size, 1)
+        self.W_C1 = nn.Linear(embedding_size + hidden_size, cell_size)
+        self.W_o1 = nn.Linear(embedding_size + hidden_size, 1)
+
+        self.W_f2 = nn.Linear(embedding_size + hidden_size, 1)
+        self.W_i2 = nn.Linear(embedding_size + hidden_size, 1)
+        self.W_C2 = nn.Linear(embedding_size + hidden_size, cell_size)
+        self.W_o2 = nn.Linear(embedding_size + hidden_size, 1)
+
+        self.output = nn.Linear(2*hidden_size, vocab_size)
+
+        self.sigmoid = nn.Sigmoid()
+        # self.softmax = nn.LogSoftmax()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        encode = self.embeddings[x.data,:]
+        seq_length = x.size()[0]
+        batch_size = x.size()[1]
+        h = self.init_hidden(batch_size)
+        C = self.init_cell(batch_size)
+        total_h1 = Variable(torch.FloatTensor(seq_length, batch_size, self.hidden_size))
+
+        for t, step in enumerate(encode):
+            total_h1[t] = h
+            # print(t)
+
+            if self.dropout and self.training:
+                step_mask = Variable(torch.bernoulli(
+                    torch.Tensor(batch_size, self.embedding_size).fill_(1. - self.dropout)), requires_grad=False) / self.dropout
+                h_mask = Variable(torch.bernoulli(
+                    torch.Tensor(batch_size, self.hidden_size).fill_(1. - self.dropout)), requires_grad=False) / self.dropout
+                C_mask = Variable(torch.bernoulli(
+                    torch.Tensor(batch_size, self.cell_size).fill_(1. - self.dropout)), requires_grad=False) / self.dropout
+                step = step * step_mask
+                h = h * h_mask
+                C = C * C_mask
+
+            if t == seq_length - 1:
+                break
+            
+            f_t = self.sigmoid(self.W_f1(torch.cat((h, step),1))).expand_as(C)
+            i_t = self.sigmoid(self.W_i1(torch.cat((h, step),1))).expand_as(C)
+            C_t1 = self.tanh(self.W_C1(torch.cat((h,step),1)))
+            C = f_t * C + i_t + C_t1
+
+            o_t = self.sigmoid(self.W_o1(torch.cat((h,step),1))).expand_as(C)
+            h = o_t * self.tanh(C)
+
+        h = self.init_hidden(batch_size)
+        total_h2 = Variable(torch.FloatTensor(seq_length, batch_size, self.hidden_size))
+        for t, step in enumerate(reversed(encode)):
+            # print(seq_length-t-1)
+            total_h2[seq_length-t-1] = h
+            if self.dropout and self.training:
+                step_mask = Variable(torch.bernoulli(
+                    torch.Tensor(batch_size, self.embedding_size).fill_(1. - self.dropout)), requires_grad=False) / self.dropout
+                h_mask = Variable(torch.bernoulli(
+                    torch.Tensor(batch_size, self.hidden_size).fill_(1. - self.dropout)), requires_grad=False) / self.dropout
+                C_mask = Variable(torch.bernoulli(
+                    torch.Tensor(batch_size, self.cell_size).fill_(1. - self.dropout)), requires_grad=False) / self.dropout
+                step = step * step_mask
+                h = h * h_mask
+                C = C * C_mask
+
+            if t == seq_length - 1:
+                break
+
+            f_t = self.sigmoid(self.W_f2(torch.cat((h, step),1))).expand_as(C)
+            i_t = self.sigmoid(self.W_i2(torch.cat((h, step),1))).expand_as(C)
+            C_t1 = self.tanh(self.W_C2(torch.cat((h,step),1)))
+            C = f_t * C + i_t + C_t1
+
+            o_t = self.sigmoid(self.W_o2(torch.cat((h,step),1))).expand_as(C)
+            h = o_t * self.tanh(C)
+
+        total_h = torch.cat((total_h1, total_h2), 2)
+        a = self.output(total_h)
+        return self.logsoftmax(a)
+
+    def logsoftmax(self, a):
+        return a - logsumexp(a, 2).expand_as(a)
+
+    def init_hidden(self, batch_size):
+        return Variable(torch.zeros(batch_size, self.hidden_size))
+
+    def init_cell(self, batch_size):
+        return Variable(torch.zeros(batch_size, self.cell_size))
